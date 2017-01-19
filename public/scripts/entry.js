28d1b94ffd990d2033e6d3b85a8918c15f4cb87f
nreddy216 <nreddy216@gmail.com>
2017-01-18 20:56:36 -0800

    first commit

---
+++ public/scripts/entry.js
@@ -0,0 +1,2 @@
+require('../styles/main.scss');
+console.log("It works ?");
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+
+
+module.exports = {
+  entry: "../public/scripts/entry.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  }
+};
