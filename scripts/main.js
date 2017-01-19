ab39dd82451e9145806a4456b989b23ba5179890
nreddy216 <nreddy216@gmail.com>
2017-01-19 00:22:32 -0800

    kind of working - needs css responsive fixes and better way to drop rain

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,36 @@
 $( document ).ready(function(){
-  console.log("Linked");
 
-  var drop = $('.drop');
+  function goldenShower(timeToDrip){
+    var $drops = $(),
+        quantity = 15;
 
-  console.log(drop);
+    for (var i = 0; i < quantity; ++i) {
+      // Create drop
+      var $drop = $("<div class='drop'><img src='./assets/drop_small.svg' alt='drop'></div>");
+
+      // Update css
+      $drop.css({
+        'margin-left': (Math.random() * $('.shower').width()) + 'px',
+        'margin-top': (- Math.random() * $('.shower').height()) + 'px'
+      });
+      // add this drop to the set of drops
+      $drops = $drops.add($drop);
+    }
+
+    $('.drop-zone').prepend($drops);
+
+    $drops.animate({
+        'margin-top': "500px",
+        'opacity' : "0",
+    }, Math.random() + 2000, function(){
+        $(this).remove();
+    });
+  }
+
+  var $showerhead = $('.showerhead');
+
+  $showerhead.on('click', function(){
+    goldenShower();
+  });
 
 });
